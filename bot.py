2479bee43ab5da9ee9c3546064ba8d1350676073
papertoy1127 <46876705+papertoy1127@users.noreply.github.com>
2020-04-09 21:38:42 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -58,4 +58,4 @@ async def on_message(message):
 #    ment = self.mention
 #    await self.get_channel(697660178190958664).send("{has joined the server.")
 access_token=os.environ["BOT_TOKEN"]
-client.run("jKKyDbVUvwvV7v4nZbZscyivJDELVwEg")
+client.run("Njk3NzMwMjE4NDYyNDEyODEw.Xo8XHw.Xb5IZjfgE07FCUicKJJuoOyUvfQ")

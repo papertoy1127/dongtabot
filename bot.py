8233f8eb79564ea98f301226d149170698f33cbe
papertoy1127 <46876705+papertoy1127@users.noreply.github.com>
2020-04-10 11:51:34 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -57,12 +57,9 @@ async def on_message(message):
     elif (message.content=="!MEE6 MEE6")&(str(message.channel)=="역할_요청"):
         await message.channel.send("뮥뮥")
     elif (message.content.split()[0]=="!me"):
-        if (message.content=="!me"):
-            await message.channel.send(message.author.mention)
-        else:
-            await message.channel.send(str(message.author.menton)+str(message.content.split(maxsplit=1)[1]))
+        await message.channel.send(message.author.mention)
     elif (message.content=="!you")&(str(message.channel)=="역할_요청"):
-        await message.channel.send(client.user.mention)
+        await message.channel.send(message.content.split(maxsplit=1)[1])
 
 @client.event
 async def on_member_join(member):
